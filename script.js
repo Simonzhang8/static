29bd86fc7d5cf7b2c1779892b6a36120ed602e89
张先淼 <165749751+Simonzhang8@users.noreply.github.com>
2024-07-26 17:39:00 +0800

    Update and rename chatra.js to script.js

---
+++ script.js
@@ -0,0 +1,17 @@
+(function(d, w, c) {
+    w.ChatraID = 'bQgDpicHW6nirusCK';
+    var s = d.createElement('script');
+    w[c] = w[c] || function() {
+        (w[c].q = w[c].q || []).push(arguments);
+    };
+    s.async = true;
+    s.src = 'https://call.chatra.io/chatra.js';
+    if (d.head) d.head.appendChild(s);
+})(document, window, 'Chatra');
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?0eb9a2de43fab88e47bb71e978c32c02";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
